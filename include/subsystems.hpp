099f93bc00c47812652829d8c4ba21eca1188b9d
Averey Ku <avereysku@gmail.com>
2024-11-11 15:44:36 -0500

    intialized repo. monty.

---
+++ include/subsystems.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "api.h"
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor intake(3);
+// inline pros::adi::DigitalIn limit_switch('A');
+
+inline pros::ADIPort clampCylinder('A', pros::E_ADI_DIGITAL_OUT);
